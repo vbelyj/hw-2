27db6401dc4f031e17978b037f19ba554cc8de30
vbelyj <93213110+vbelyj@users.noreply.github.com>
2021-12-06 19:45:20 +0200

    Create NameEncoderDecoder.java

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,24 @@
+class NameEncoderDecoder {
+    
+  public String encode(String name) {
+  String encodeName =
+                "NOTFORYOU" + name.replace ("e", "1")
+                        .replace ("u", "2")
+                        .replace ("i", "3")
+                        .replace ("o", "4")
+                        .replace ("a", "5")
+                        + "YESNOTFORYOU";
+        return encodeName;
+  }
+  
+  public String decode(String name) {
+        String decodeName = name.replace ("YESNOTFORYOU", "")
+                .replace ("NOTFORYOU", "")
+                .replace ("1", "e")
+                .replace ("2", "u")
+                .replace ("3", "i")
+                .replace ("4", "o")
+                .replace ("5", "a");
+        return decodeName;
+    }
+}
